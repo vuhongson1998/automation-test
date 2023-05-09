1a83fae1181116250bd96de84a74ba2985609a86
sonvh98 <sonvh98os@gmail.com>
2023-05-09 10:49:01 +0700

    Automation test1

---
+++ tests/StaffTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+class StaffTest extends TestCase
+{
+	public function testReturnsFullName()
+	{
+		require('Staff.php');
+		$staff = new Staff;
+		$staff->first_name = "Nguyen";
+		$staff->last_name = "Bao";
+		$this->assertEquals('Nguyen Bao', $staff->getFullName());
+	}
+	
+	public function testFullNameIsEmptyByDefault()
+	{
+		$staff = new Staff;
+		$this->assertEquals('', $staff->getFullName());
+	}
+	
+	public function testOnlyFirstName()
+	{
+		$staff = new Staff;
+		$staff->first_name = "Nguyen";
+		$this->assertEquals('Nguyen', $staff->getFullName());
+	}
+}
+?>
